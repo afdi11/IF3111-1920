87b7518cd3c0bfc18d6ce54c7cda45dc0c0421f8
alien087 <44940977+alien087@users.noreply.github.com>
2019-11-28 01:05:27 +0700

    Memperbaharui Landing Page
    Mengganti background dan menambahkan How to

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,6 +7,7 @@
   <section class="home">
     <h1 style="">Layanan Aspirasi Masyarakat ITERA</h1>
     <h2>Sampaikan laporan Kamu langsung kepada KM ITERA</h2>
+
     <div class="wrap">
      <div class="search">
         <input type="text" class="searchTerm" placeholder="Masukkan Kata Kunci Atau ID Laporan">
@@ -15,6 +16,22 @@
        </button>
      </div>
    </div>
+
+   <div class="lapor" >
+
+     <button type="button" name="button" class="tambah"><i class="fa fa-plus"> </i> Tambah Laporan</button>
+   </div>
+
+  </section>
+
+  <section class="container">
+    <h1 style="padding-top: 0; padding-bottom: 1em;">Cara Mengirim Laporan</h1>
+    <ul class="progressbar">
+     <li class="active" >Tulis Laporan</li>
+     <li>Proses Verifikasi</li>
+     <li>Proses Tindak Lanut</li>
+     <li>Selesai</li>
+   </ul>
   </section>
 
 @endsection
