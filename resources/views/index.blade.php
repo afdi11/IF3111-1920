7bc60dbd046ac0671bf8a63484a21dd8cc8e6c22
alien087 <44940977+alien087@users.noreply.github.com>
2019-11-27 22:32:29 +0700

    Landing Page Awal
    Landing Page Awal baru judul search bar statis

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,20 @@
 @extends('layout/main')
 
 @section('title', 'Lapor ITERA')
+
+@section('container')
+
+  <section class="home">
+    <h1 style="">Layanan Aspirasi Masyarakat ITERA</h1>
+    <h2>Sampaikan laporan Kamu langsung kepada KM ITERA</h2>
+    <div class="wrap">
+     <div class="search">
+        <input type="text" class="searchTerm" placeholder="Masukkan Kata Kunci Atau ID Laporan">
+        <button type="submit" class="searchButton">
+          <i class="fa fa-search" style=""></i>
+       </button>
+     </div>
+   </div>
+  </section>
+
+@endsection
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -3,13 +3,15 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>@yield('title')</title>
   </head>
 
   <body>
-    <div class="header" style="margin: 0;">
+
+    <div id="myHeader" class="header" style="margin: 0;">
     <header>
-      <img class="logo" src="{{ asset('image/logo.png') }}" style="width: 15%" alt="">
+      <img href="/" class="logo" src="{{ asset('image/logo.png') }}" style="width: 15%" alt="">
       <nav>
         <ul class="nav__links">
           <li> <a href="/">Home</a> </li>
@@ -21,7 +23,21 @@
     </header>
   </div>
 
+  @yield('container')
 
+  <script type="text/javascript">
+  window.onscroll = function() {myFunction()};
 
+  var header = document.getElementById("myHeader");
+  var sticky = header.offsetTop;
+
+  function myFunction() {
+    if (window.pageYOffset > sticky) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+  }
+  </script>
   </body>
 </html>
