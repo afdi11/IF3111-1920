ad7ff3d5a7ad08fc601d6cf8f5e207b53da23891
Afdi Fauzul Bahar <45322055+afdi11@users.noreply.github.com>
2019-12-08 22:17:59 +0700

    ADD : Buat view untuk buat laporan

--- resources/views/buat_laporan.blade.php
+++ resources/views/buat_laporan.blade.php
@@ -3,3 +3,35 @@
 @section('title', 'Buat Laporan')
 
 @section('container')
+
+<section class="home">
+    <div class="home-box">
+    <h1 style="">Layanan Aspirasi Masyarakat ITERA</h1>
+    <h2>Sampaikan laporan/komentarmu!</h2>
+    </div>
+</section>
+
+<section class="containers">
+    <div class="container">
+    <div class="form-lapor">
+    <form method="POST" ation="#" >
+        <input name="pelapor" type=hidden value="####"></input>
+        <div class="form-lapor-isi">
+            <textarea name="isi" class="form-control textarea-flex autosize" placeholder="Ketik laporan anda..." style="overflow: hidden; overflow-wrap: break-word; height: 210px;"></textarea>
+        </div>
+        <div class="form-lapor-kategori">
+            <select name="aspek">
+                <option value=1>Dosen</option>
+                <option value=2>Mahasiswa</option>
+                <option value=3>Infrastruktur</option>
+            </select>
+        </div>
+    </form>
+    </div>
+    </div>
+</section>
+
+<section class="hangat">
+</section>
+
+@endsection
