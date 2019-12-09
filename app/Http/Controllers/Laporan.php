1ad23772b58dacf914d5096d0462a4d5220530e1
Afdi Fauzul Bahar <45322055+afdi11@users.noreply.github.com>
2019-12-09 10:38:14 +0700

    UPDATE Laporan.php : Menambahkan fungsi insert into database dan passing data

---
+++ app/Http/Controllers/Laporan.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\User;
+
+
+class Laporan extends Controller
+{
+     function form(){
+      $data['data'] = DB::table('laporan')->get();
+      $data['aspek'] = DB::table('aspek')->get();
+        return view('buat_laporan', $data);
+     }
+     public function tambah(Request $request){
+      DB::table('laporan')->insert([
+        'Pelapor'=>$request->pelapor,
+        'isi'=>$request->isi,
+        'aspek'=>$request->aspek,
+        'lampiran'=>$request->Lampiran,
+        'Waktu'=>NOW()
+        ]);
+      return redirect('/laporan');
+     }
+}
+?>
