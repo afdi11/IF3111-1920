c1d7b03245aa458ff8ef1fa9286dab0a4e2340db
Afdi Fauzul Bahar <45322055+afdi11@users.noreply.github.com>
2019-12-09 10:33:17 +0700

    UPDATE buat_laporan.blade.php : Buat form yang sudah bisa di submit

--- resources/views/buat_laporan.blade.php
+++ resources/views/buat_laporan.blade.php
@@ -14,18 +14,25 @@
 <section class="containers">
     <div class="container">
     <div class="form-lapor">
-    <form method="POST" ation="#" >
-        <input name="pelapor" type=hidden value="####"></input>
+    <form method="POST" action="/laporan/tambah" >
+        {{ csrf_field() }}
+        <input name="pelapor" type=hidden value="anonim"></input>
+        
         <div class="form-lapor-isi">
-            <textarea name="isi" class="form-control textarea-flex autosize" placeholder="Ketik laporan anda..." style="overflow: hidden; overflow-wrap: break-word; height: 210px;"></textarea>
+            <textarea name="isi" placeholder="Ketik laporan anda..." style="overflow: hidden; overflow-wrap: break-word; height: 210px;"></textarea>
         </div>
-        <select name="aspek" placeholder="Pilihan Kategori">
-            <option value=1>Dosen</option>
-            <option value=2>Mahasiswa</option>
-            <option value=3>Infrastruktur</option>
+
+        <select name="aspek" style="width:100%; height:40px">
+            <option value="" hidden>Pilih aspek laporan</option>
+            @foreach($aspek as $a)
+                <option value="{{$a->ID}}">{{$a->Jenis}}</option>
+            @endforeach
         </select><br/><br/>
-        <input name="Lampiran" type="file" accept="image/*"><br/><br/>
-        <input type="submit"><br/>
+        <input name="Lampiran" type="file" accept="image/*">
+
+        <div class="lapor-submit">
+            <input class="lapor-button" type="submit" value="LAPOR!"></input>
+        </div>
     </form>
     </div>
     </div>
